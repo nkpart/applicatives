246540fd7b14bf30cb1028992ff7b4a8942cf084
Nick Partridge <nkpart@gmail.com>
2015-04-22 17:54:15 +1000

    .

---
+++ README.md
@@ -0,0 +1,11 @@
+applicatives
+====
+
+`mtl`-style typeclasses for working with compositions of applicatives
+
+TODO
+---
+
+* explain instance selection via the compose newtype
+* products?
+
--- src/Data/Functor/Compose/Where.hs
+++ src/Data/Functor/Compose/Where.hs
@@ -1,24 +1,21 @@
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE FunctionalDependencies #-}
-{-# LANGUAGE FlexibleInstances #-}
-
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE UndecidableInstances #-}
 module Data.Functor.Compose.Where where
-
-import Data.Functor.Compose
 
 data OnRight
 data OnLeft
 data Nowhere
 
-type family Or a b where
-              Or Nowhere Nowhere = Nowhere
-              Or Nowhere f = OnRight
-              Or f Nowhere = OnLeft
 
+-- | Find a type within a product of types
+--   eg. WhereIs IO Compose (Compose IO Identity) ==> OnLeft
 type family WhereIs x (c :: (* -> *) -> (* -> *) -> * -> *) (z :: * -> *) where
   WhereIs x c (c x f) = OnLeft
   WhereIs x c (c f x) = OnRight
   WhereIs x c (c f g) = Or (WhereIs x c f) (WhereIs x c g)
   WhereIs x c f = Nowhere
+
+type family Or a b where
+              Or Nowhere Nowhere = Nowhere
+              Or Nowhere f = OnRight
+              Or f Nowhere = OnLeft
